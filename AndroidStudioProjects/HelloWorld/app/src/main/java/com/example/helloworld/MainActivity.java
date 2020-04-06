0c85c698704dcd5ed8ed24b98c81c121dc7e0ff6
Kakkatykki <36374215+kakkatykki@users.noreply.github.com>
2020-04-06 09:46:18 +0300

    Hello world v final

--- AndroidStudioProjects/HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
+++ AndroidStudioProjects/HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.helloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Environment;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.KeyEvent;
@@ -13,20 +15,26 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import java.lang.reflect.Field;
+import java.sql.SQLOutput;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button changeButton;
-    TextView helloField;
+    Button changeButton, saveButton, loadButton;
+    TextView textField;
+    EditText helloEditor, fileName;
 
-    EditText helloEditor;
-
+    Context c;
+    FileHandler filukka;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         // Setup view
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        c = getApplicationContext();
+        filukka = new FileHandler(c);
         initButtons();
         initFields();
         initEditors();
@@ -37,18 +45,50 @@ public class MainActivity extends AppCompatActivity {
         changeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                onClickEvent();
+                changeText();
+            }
+        });
+
+        saveButton = (Button)findViewById(R.id.button_save);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveText();
+            }
+        });
+
+        loadButton = (Button)findViewById(R.id.button_load);
+        loadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadText();
             }
         });
     }
 
-    void onClickEvent() {
+    void changeText() {
         System.out.println("_LOG: Hello World");
-        helloField.setText(helloEditor.getText());
+        textField.setText(helloEditor.getText());
+    }
+
+    void saveText() {
+        if (filukka != null)
+            filukka.writeFile(fileName.getText().toString(), textField.getText().toString());
+        System.out.println("_LOG:" + filukka);
+    }
+
+    void loadText() {
+        ArrayList<String> fileLines = new ArrayList<String>();
+        String contents = "";
+        fileLines = filukka.readFile(fileName.getText().toString());
+        for (String s : fileLines) {
+            contents += s;
+        }
+        textField.setText(contents);
     }
 
     void initFields() {
-        helloField = (TextView)findViewById(R.id.field_hello_world);
+        textField = (TextView)findViewById(R.id.field_hello_world);
     }
 
     void initEditors() {
@@ -61,7 +101,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-                helloField.setText(helloEditor.getText());
+                textField.setText(helloEditor.getText());
             }
 
             @Override
@@ -70,6 +110,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        fileName = (EditText)findViewById(R.id.edit_text_filename);
 
     }
 }
